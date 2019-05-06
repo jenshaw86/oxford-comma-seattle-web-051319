7bd5022195573b6ac4058cab0ba69a91caa94576
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 05:13:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,8 +7,8 @@ def oxford_comma(array)
   elsif items >= 3
     list = ""
     item_count = 0
-    until item == array.length - 2
-      list.concat("#{array[item]}, ")
+    until item_count == array.length - 2
+      list.concat("#{array[item_count]}, ")
       item_count += 1
     end
     list.concat(" and #{array.last}")
