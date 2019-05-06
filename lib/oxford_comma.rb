25e91c0b673f4e2eefcd6ffa0d7428d6fac96d71
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 05:07:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,12 @@
 def oxford_comma(array)
-  list = ""
+  
   case array.length
   when 1
     list = array.first
   when 2
     list = array.join(" and ")
   when array.length >= 3
+    list = ""
     item = 0
     until item == array.length - 2
       list.concat("#{array[item]}, ")
@@ -13,5 +14,4 @@ def oxford_comma(array)
     end
     list.concat(" and #{array.last}")
   end
-  puts list
 end
